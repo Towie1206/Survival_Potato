3c8ce1b8f7b5ac426296c7e0f48c21970cda78f6
Nguyen Cong Bang <ncongbang2006@gmail.com>
2026-05-29 00:54:22 +0700

    Basic Move and some Camera

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Vector2 minmaxXY;
+    PlayerController player = PlayerController.Instance;
+
+    private void LateUpdate()
+    {
+        Vector3 target = new Vector3(PlayerController.Instance.transform.position.x, PlayerController.Instance.transform.position.y, -10);
+        //giới hạn giá trị 1 khoảng nhất định Mathf.Clamp(giá_trị_cần_kiểm_tra, min, max);
+       target.x = Mathf.Clamp(PlayerController.Instance.transform.position.x, -minmaxXY.x, minmaxXY.x); 
+       target.y = Mathf.Clamp(PlayerController.Instance.transform.position.y, -minmaxXY.y, minmaxXY.y);
+
+        transform.position = target;
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController Instance;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    [SerializeField] float speed = 1.2f;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        movement = new Vector2(inputX, inputY).normalized;
+    }
+    void FixedUpdate()
+    {
+       rb.linearVelocity = new Vector2(movement.x * speed, movement.y * speed);
+    }
+}
